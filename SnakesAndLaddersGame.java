03ff33e24293b617b682157a18c2b9a7d12322f0
Your Name <you@example.com>
2020-10-21 23:49:35 +0530

    Added:Position of Player

--- SnakesAndLaddersGame.java
+++ SnakesAndLaddersGame.java
@@ -1,5 +1,7 @@
 public class SnakesAndLaddersGame{
+static int FirstPlayer=0;
 	public static void main(String args[]){
 		System.out.println("*****WELCOME TO SNAKES AND LADDERS GAME*****");
+		System.out.println("Position of First Player is at "+FirstPlayer);
 	}
 }
